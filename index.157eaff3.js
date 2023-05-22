f2d27e16f1e482090797de4c0031cfe55f545464
IraMira333 <119503034+IraMira333@users.noreply.github.com>
2023-05-22 12:51:58 +0000

    Deploying to gh-pages from @ IraMira333/goit-js-hw-11-2@8a3ebf9dc2cae7c538c83c100dfa364f51030ebe 🚀

--- index.157eaff3.js
+++
@@ -1,2 +0,0 @@
-fetch("".concat("https://pixabay.com/api/","?key=").concat("36597774-2e36ba17425207cc180a817c1","&q=").concat("","&").concat("image_type=photo&orientation=horizontal&safesearch=true"));
-//# sourceMappingURL=index.157eaff3.js.map
