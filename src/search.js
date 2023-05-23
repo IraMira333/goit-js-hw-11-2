35932e2efbf7ecf330d719442e50417910161a1e
IraMira333 <irinat333@gmail.com>
2023-05-23 15:09:46 +0300

    Update search.js

--- src/search.js
+++ src/search.js
@@ -5,9 +5,9 @@ const params =
 
 //https://pixabay.com/api/?key=36597774-2e36ba17425207cc180a817c1&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true
 
-function searchFoto(query) {
-  return fetch(`${URL}?key=${API_KEY}&q=${query}&${params}`).then(resp =>
-    resp.json()
+function searchFoto(query, page = 1) {
+  return fetch(`${URL}?key=${API_KEY}&q=${query}&${params}&page=${page}`).then(
+    resp => resp.json()
   );
 }
 
