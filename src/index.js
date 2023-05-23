c291a5536f826cc02beb308ba66303949919b3d1
IraMira333 <irinat333@gmail.com>
2023-05-23 16:07:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,28 @@
-import API from './search.js';
 import { Notify } from 'notiflix';
 import { createMarkup } from './createMarkup.js';
+import SearchImageService from './SearchImageService.js';
+
 const refs = {
   formEl: document.getElementById('search-form'),
   imagesEl: document.querySelector('.gallery'),
   loadMoreEl: document.querySelector('.load-more'),
 };
 
-let page = 1;
-let searchValue = '';
+const searchImageService = new SearchImageService();
 
 refs.formEl.addEventListener('submit', onSubmit);
 refs.loadMoreEl.addEventListener('click', onLoadMore);
+
 function onSubmit(evt) {
   evt.preventDefault();
   const value = evt.currentTarget.elements.searchQuery.value.trim();
   console.log(value);
   if (value === '') return Notify.failure('No search query!');
   else {
-    searchValue = value;
-    page = 1;
-
-    API.searchFoto(value)
-      .then(({ hits }) => {
-        console.log(hits);
-        if (hits.length === 0)
-          throw new Error(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        return hits.reduce((markup, hit) => markup + createMarkup(hit), '');
-      })
-      .then(updateDivGallery)
-      .catch(onError);
+    searchImageService.query = value;
+    searchImageService.page = 1;
+    clearDivGallery();
+    getDataForMarkup();
   }
 }
 
@@ -41,10 +32,27 @@ function onError(err) {
 }
 
 function updateDivGallery(markup) {
-  refs.imagesEl.innerHTML = markup;
+  refs.imagesEl.insertAdjacentHTML('beforeend', markup);
+}
+function clearDivGallery() {
+  refs.imagesEl.innerHTML = '';
 }
 
 function onLoadMore() {
-  page += 1;
-  API.searchFoto(searchValue, page);
+  getDataForMarkup();
+}
+
+function getDataForMarkup() {
+  return searchImageService
+    .searchFoto()
+    .then(hits => {
+      console.log(hits);
+      if (hits.length === 0)
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      return hits.reduce((markup, hit) => markup + createMarkup(hit), '');
+    })
+    .then(updateDivGallery)
+    .catch(onError);
 }
