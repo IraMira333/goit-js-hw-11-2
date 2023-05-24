cfc268adc4c068330c817bdc57ef68b983b26909
IraMira333 <irinat333@gmail.com>
2023-05-24 13:53:29 +0300

    Update SearchImageService.js

--- src/SearchImageService.js
+++ src/SearchImageService.js
@@ -1,20 +1,26 @@
+import { Notify } from 'notiflix';
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '36597774-2e36ba17425207cc180a817c1';
-const params =
-  'image_type=photo&orientation=horizontal&safesearch=true&per_page=6';
+const params = 'image_type=photo&orientation=horizontal&safesearch=true';
 
 export default class SearchImageService {
   constructor() {
     this.page = 1;
     this.query = '';
+    this.per_page = 6;
   }
   searchFoto() {
     return fetch(
-      `${URL}?key=${API_KEY}&q=${this.query}&${params}&page=${this.page}`
+      `${URL}?key=${API_KEY}&q=${this.query}&${params}&page=${this.page}&per_page=${this.per_page}`
     )
       .then(resp => resp.json())
-      .then(({ hits }) => {
+      .then(({ hits, total }) => {
+        if (this.page === 1 && total !== 0) {
+          console.log(total);
+          Notify.info(`Hoorey! We found ${total} images`);
+        }
         this.incrementPage();
+
         return hits;
       });
   }
